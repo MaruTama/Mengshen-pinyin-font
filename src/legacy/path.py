633a15133793cda4d52e74fbdfa6eed80126f03c
Yuya Maruyama <marutama.valentine@gmail.com>
2025-07-13 19:20:02 +0900

    chore: apply path of legacy code

--- src/legacy/path.py
+++ src/legacy/path.py
@@ -3,12 +3,14 @@ from pathlib import Path
 
 DIR = Path(__file__).parent
 
-DIR_OUTPUT = os.path.normpath(os.path.join(DIR, "../outputs/"))
-DIR_TEMP = os.path.normpath(os.path.join(DIR, "../tmp/json/"))
+DIR_OUTPUT = os.path.normpath(os.path.join(DIR, "../../outputs/"))
+DIR_TEMP = os.path.normpath(os.path.join(DIR, "../../tmp/json/"))
 
-DIR_FONT_FOR_HAN_SERIF = os.path.normpath(os.path.join(DIR, "../res/fonts/han-serif"))
+DIR_FONT_FOR_HAN_SERIF = os.path.normpath(
+    os.path.join(DIR, "../../res/fonts/han-serif")
+)
 DIR_FONT_FOR_HANDWRITTEN = os.path.normpath(
-    os.path.join(DIR, "../res/fonts/handwritten")
+    os.path.join(DIR, "../../res/fonts/handwritten")
 )
 
-DIR_PHONICS = os.path.normpath(os.path.join(DIR, "../res/phonics/"))
+DIR_PHONICS = os.path.normpath(os.path.join(DIR, "../../res/phonics/"))
